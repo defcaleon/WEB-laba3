f88e713304977444221d8d4816a46e4f883db02d
Nikita Parmon <nikitka.parmon@mail.ru>
2021-03-05 11:38:09 +0300

    laba 3 with dop

--- scripts/laba3/laba3_php.php
+++ scripts/laba3/laba3_php.php
@@ -1,18 +1,47 @@
     <?php
-    try {
+        
+        $age=getAgeByDate($_POST["text"]);
+        days10000($_POST["text"]);
+
+    	try{
     	
-    	if($_POST["text"]==null){
-    		throw new Exception('empty text');
-    	}
+        echo $age->format('%y years %m month %d days')."<br/>";
+        echo $age->format('total days: %a ')."<br/>";
+        }
+         catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        
+
+
+    function getAgeByDate($date)
+    {
+        
+
+        if($date==null){
+            throw new Exception('empty text');
+            return;
+        }
 
         $currDate= new DateTime('now');
-        $date= new DateTime($_POST["text"]);
+        $date= new DateTime($date);
+        if($currDate<$date){
+            throw new Exception('future date');
+            return;
+        }
+
         $age = date_diff($currDate,$date);
-        echo $age->format('%y years %m month, %d days');
+        return $age;
 
     }
-    catch (Exception $e) {
-    	echo $e->getMessage();
+
+    function days10000($date){
+
+            $date= new DateTime($date);
+            date_add($date, date_interval_create_from_date_string("10000 days"));
+            echo $date->format('Y-m-d');
+            echo "<br/>";
+        
+
     }
 
-    ?>
